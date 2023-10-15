1b807f536b9df0c2cf503ad0779cc91c59c4e0ea
Neethu Cleetus <neethuraphel1992@gmail.com>
2023-10-15 22:04:17 +0530

    index and package

---
+++ index.js
@@ -0,0 +1,53 @@
+import {
+    orkesConductorClient,
+    ConductorWorker,
+    TaskManager
+  } from "@io-orkes/conductor-javascript";
+  
+  
+  const config = {
+    keyId: process.env.KEY,
+    keySecret: process.env.SECRET,
+    serverUrl: process.env.SERVER_URL,
+  };
+  
+  console.log(config);
+  
+  const clientPromise = orkesConductorClient(config);
+  
+  const client = await clientPromise;
+  
+  const taskDefName = "HelloWorldWorker";
+  
+  const customWorker: ConductorWorker = {
+  taskDefName,
+    execute: async ({ inputData, taskId }) => {
+      return {
+        outputData: {
+          greeting: "Hello World",
+        },
+        status: "COMPLETED",
+      };
+    },
+  };
+  // Worker Options will take precedence over options defined in the manager
+  
+  const manager = new TaskManager(client, [customWorker], {
+    options: { pollInterval: 100, concurrency: 1 },
+  });
+  
+  manager.startPolling();
+  // You can update all worker settings at once using
+  manager.updatePollingOptions({ pollInterval: 100, concurrency: 1 });
+  
+  // You can update a single worker setting using :
+  manager.updatePollingOptionForWorker(taskDefName, {
+    pollInterval: 100,
+    concurrency: 1,
+  });
+  
+  manager.isPolling // Will resolve to true
+  
+  await manager.stopPolling();
+  
+  manager.isPolling // Will resolve to false
